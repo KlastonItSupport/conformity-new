f9f9367a6027f8da9e452462727a0dddece37799
gustavool1 <go8895806@gmail.com>
2024-06-07 11:06:46 -0300

    feat: showing key to test, but ill delete it later

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:3000/",
-  baseURL: "https://conformity-new-api-production.up.railway.app/",
+  baseURL: "http://localhost:3000/",
+  // baseURL: "https://conformity-new-api-production.up.railway.app/",
 });
