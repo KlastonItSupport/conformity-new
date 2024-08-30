57cd34092ad81b13ddc7e47033c8fcc746e5eca8
gustavool1 <go8895806@gmail.com>
2024-08-30 10:39:24 -0300

    feat: changing api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://93.127.210.174:3000/",
+  baseURL: "https://api.klaston.com/",
   // baseURL: "http://localhost:3000/",
   // baseURL: "https://conformity-new-api-production.up.railway.app/",
 });
